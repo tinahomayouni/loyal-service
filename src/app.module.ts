b8684ee6f25ab5bae878390c3b114b57635bbe84
tinahomayouni <tinahomayouni@gmail.com>
2024-10-16 12:24:47 +0330

    add app.module elements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+  ConfigModule.forRoot({ isGlobal: true }),
+  DatabaseModule,
+  UserModule,
+  AuthModule
+],
   controllers: [],
   providers: [],
 })
