d64738c2daf6b3e1d666a361e50231573f86b0d4
tinahomayouni <tinahomayouni@gmail.com>
2024-10-16 14:42:51 +0330

    add badge. level, dates to table(entity) user

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -15,6 +15,12 @@ export class User {
   @MinLength(6)
   password: string;
 
+  @Column({ type: 'int', default: 1 })
+  level: number; // Level will be 1, 2, or 3
+
+  @Column({ type: 'varchar', default: 'bronze' })
+  badge: string; // Badge will be 'bronze', 'silver', or 'gold'
+
   @CreateDateColumn()
   createdAt: Date;
 
