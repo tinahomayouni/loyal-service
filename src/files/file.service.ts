5080fbf780aee3c04afef510ceffd2b5cdacc5fd
tinahomayouni <tinahomayouni@gmail.com>
2024-10-20 15:54:03 +0330

    set comment and delete images from uploads

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -6,18 +6,6 @@ import * as path from 'path';
 @Injectable()
 export class FilesService {
   async uploadFile(file: Express.Multer.File) {
-    // Add your specific file upload logic here, including:
-
-    // 1. Validation (e.g., file size, type, MIME type)
-    // ...
-
-    // 2. Custom filename generation (optional)
-    // ...
-
-    // 3. Advanced storage logic (optional)
-    // ...
-
-    // 4. Return relevant information or perform further actions
     return {
       originalname: file.originalname,
       filename: file.filename,
